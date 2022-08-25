2741aa919d57b55b3244d461b0c590df3c766dbd
Lawrence Dovin <lawrencedovin@gmail.com>
2022-08-25 18:29:15 -0400

    Updated shopping cart navbar spacing

--- shoply/src/Navbar.js
+++ shoply/src/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { calculateTotalQuantity } from "./helpers";
+import './Item.css';
 
 function Navbar() {
   const itemCount = useSelector(st => calculateTotalQuantity(st.cartItems));
@@ -9,21 +10,23 @@ function Navbar() {
   const itemUnit = itemCount === 1 ? "item" : "items";
   return (
     <nav className="navbar navbar-light bg-info">
-      <Link to="/" className="navbar-brand text-light">
-        SHOPLY!
-      </Link>
-      <ul className="navbar-nav flex-row">
-        <li className="nav-item pr-3">
-          <span className="navbar-text text-light">
-            {itemCount} {itemUnit} (${cartValue})
-          </span>
-        </li>
-        <li className="nav-item">
-          <Link to="/cart" className="nav-link text-light">
-            See Cart
-          </Link>
-        </li>
-      </ul>
+      <div className="container">
+        <Link to="/" className="navbar-brand text-light">
+          SHOPLY!
+        </Link>
+        <ul className="navbar-nav flex-row">
+          <li className="nav-item shopping-cart">
+            <span className="navbar-text text-light">
+              {itemCount} {itemUnit} (${cartValue})
+            </span>
+          </li>
+          <li className="nav-item">
+            <Link to="/cart" className="nav-link text-light">
+              See Cart
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
